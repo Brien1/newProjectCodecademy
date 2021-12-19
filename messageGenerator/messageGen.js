6abd3155842fa83fb7bd4729cbc522f6983024b3
Brien <brienhall@hotmail.co.uk>
2021-12-19 13:26:53 +0000

    finished sentence generator

--- messageGenerator/messageGen.js
+++ messageGenerator/messageGen.js
@@ -1,5 +1,14 @@
 let v = "variable";
 console.log('Hello world!');
 let random = (Math.random(5));
-random = 100*random
-console.log(Math.round(random))
+
+const noun = ["Dog", "Cat", "Man", "Woman", "Child", "Yeti", "Ewok", "Lemming"]
+const verb = ["eats", "breaks", "pushes", "hits", "bumps"]
+const thing = ["bike", "dohnut", "village idiot"]
+
+let randomNoun = noun[Math.round(random * (noun.length-1))] 
+let randomVerb = verb[Math.round(random * (verb.length-1))]
+let randomThing = thing[Math.round(random * (thing.length-1))]
+
+console.log(randomNoun+" "+randomVerb+" the "+randomThing)
+    
